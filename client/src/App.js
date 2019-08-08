fd294fa686bc0c97fe7b27849f2f52fb8ea2401c
Alex <suarez9093@hotmail.com>
2019-08-08 14:42:26 -0600

    got Auth0 to work but styling is no longer displaying

--- client/src/App.js
+++ client/src/App.js
@@ -4,21 +4,58 @@ import Books from "./pages/Books";
 import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
+import { useAuth0 } from "./react-auth0-wrapper";
+import Profile from "./components/Profile";
 
 function App() {
+  const { loading } = useAuth0();
+  if (loading) {
+    return (
+      <div>Loading...</div>
+    );
+  }
   return (
     <Router>
-      <div>
+      <div className="App">
+
         <Nav />
         <Switch>
           <Route exact path="/" component={Books} />
           <Route exact path="/books" component={Books} />
           <Route exact path="/books/:id" component={Detail} />
           <Route component={NoMatch} />
+          <Route path="/" exact />
+          <Route path="/profile" component={Profile} />
         </Switch>
+       
       </div>
     </Router>
   );
 }
 
 export default App;
+
+
+// =====================================================
+
+
+
+// function App() {
+//   const { loading } = useAuth0();
+
+//   if (loading) {
+//     return (
+//       <div>Loading...</div>
+//     );
+//   }
+
+//   return (
+//     <div className="App">
+//       <header>
+//         <Nav />
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,13 +1,47 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-function Nav() {
+// function Nav() {
+//   return (
+//     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+//       <a className="navbar-brand" href="/">
+//         React Reading List
+//       </a>
+//     </nav>
+//   );
+// }
+
+// export default Nav;
+
+// src/components/NavBar.js
+
+
+import { useAuth0 } from "../../react-auth0-wrapper";
+
+const Nav = () => {
+  const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
+
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <a className="navbar-brand" href="/">
-        React Reading List
-      </a>
-    </nav>
+    <div>
+      {!isAuthenticated && (
+        <button
+          onClick={() =>
+            loginWithRedirect({})
+          }
+        >
+          Log in
+        </button>
+      )}
+
+      {isAuthenticated && <button onClick={() => logout()}>Log out</button>}
+      {isAuthenticated && (
+      <span>
+        <Link to="/">Home</Link>&nbsp;
+        <Link to="/profile">Profile</Link>
+      </span>
+    )}
+    </div>
   );
-}
+};
 
 export default Nav;
