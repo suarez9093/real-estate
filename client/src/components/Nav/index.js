b392f4d284fae7adc96d1b4be8972a9a3979a091
Alex <suarez9093@hotmail.com>
2019-08-10 11:27:59 -0600

    starting over

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,31 +1,13 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { useAuth0 } from "../../react-auth0-wrapper";
 
-const Nav = () => {
-  const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
-
+function Nav() {
   return (
-    <div>
-      {!isAuthenticated && (
-        <button
-          onClick={() =>
-            loginWithRedirect({})
-          }
-        >
-          Book an appointment
-        </button>
-      )}
-
-      {isAuthenticated && <button onClick={() => logout()}>Log out</button>}
-      {isAuthenticated && (
-      <span>
-        <Link to="/">Home</Link>&nbsp;
-        <Link to="/profile">Profile</Link>
-      </span>
-    )}
-    </div>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+      <a className="navbar-brand" href="/">
+        React Reading List
+      </a>
+    </nav>
   );
-};
+}
 
 export default Nav;
