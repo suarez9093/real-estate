08e937f68598bc6568eff3f7b5ad2a7430d9c99b
Alex <suarez9093@hotmail.com>
2019-08-10 11:49:45 -0600

    react full stack

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,24 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Books from "./pages/Books";
+import Inmates from "./pages/Inmates";
 import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
 
 function App() {
-  return (
-    <Router>
-      <div>
-        <Nav />
-        <Switch>
-          <Route exact path="/" component={Books} />
-          <Route exact path="/books" component={Books} />
-          <Route exact path="/books/:id" component={Detail} />
-          <Route component={NoMatch} />
-        </Switch>
-      </div>
-    </Router>
-  );
+    return (
+        <Router>
+            <div>
+                <Nav />
+                <Switch>
+                    <Route exact path="/" component={Inmates} />
+                    <Route exact path="/inmates" component={Inmates} />
+                    <Route exact path="/inmates/:id" component={Detail} />
+                    <Route component={NoMatch} />
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
