6b355377e2b7955c38853b89008a5ba480ff8c17
Kalob <kalobcarter@Kalobs-MacBook-Pro.local>
2019-08-15 19:29:55 -0600

    idk

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,10 @@
 const router = require("express").Router();
 const bookRoutes = require("./books");
+const lockRoutes = require('./locks');
 
 // Book routes
 router.use("/books", bookRoutes);
+router.use('/lockitron', lockRoutes);
+
 
 module.exports = router;
