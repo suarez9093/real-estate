8ddfaa2e8446527579bfe82177f74346ed59ee46
michaeladamstrickland <strickland1514@gmail.com>
2019-08-23 14:45:01 -0600

    build

---
+++ express-api/build/precache-manifest.da8dab6faade875c89a9e3906e9631b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/static/media/logo.495d93cc.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "99e75e7f0bb4c46343b2",
+    "url": "/static/js/main.182f15cb.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "99e75e7f0bb4c46343b2",
+    "url": "/static/css/main.c0963d82.chunk.css"
+  },
+  {
+    "revision": "846138e6fa50d24ff04ac584cf41c7b9",
+    "url": "/index.html"
+  }
+];
